93b3ed7a551bc7a210449558bfe2bcd5634fa2ea
manjil <manjilkoju09@gmail.com>
2025-02-03 22:40:35 +0545

    changed home screen to user UserBloc for fetching users

--- lib/bloc/user_bloc.dart
+++ lib/bloc/user_bloc.dart
@@ -4,7 +4,11 @@ import 'package:flutter_tutorial/bloc/user_state.dart';
 import 'package:flutter_tutorial/models/user.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
-  UserBloc() : super(UserState([])) {
+  UserBloc()
+      : super(UserState([
+          User(name: "Name1", bio: "Bio1"),
+          User(name: "New User", bio: "Bio2 as he left the garden."),
+        ])) {
     on<AddUserEvent>((event, emit) {
       List<User> updatedUsers = List<User>.from(state.users)..add(event.user);
       emit(UserState(updatedUsers));
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,30 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorial/models/user.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_tutorial/bloc/user_bloc.dart';
+import 'package:flutter_tutorial/bloc/user_state.dart';
 import 'package:flutter_tutorial/widgets/profile_card.dart';
 
 class HomeScreen extends StatelessWidget {
-  final List<User> users = [
-    User(
-      name: "Name1",
-      bio: "Bio1",
-    ),
-    User(
-      name: "New User",
-      bio: "Bio2 as he left the garden.",
-    ),
-    User(
-      name: "User125",
-      bio: "Bio3 he stumbled upon a golden treasure box.",
-    ),
-  ];
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: users.length,
-      itemBuilder: (context, index) {
-        return ProfileCard(
-          user: users[index],
+    return BlocBuilder<UserBloc, UserState>(
+      builder: (context, state) {
+        return ListView.builder(
+          itemCount: state.users.length,
+          itemBuilder: (context, index) {
+            return ProfileCard(
+              user: state.users[index],
+            );
+          },
         );
       },
     );
